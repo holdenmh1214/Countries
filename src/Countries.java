bf7e63ac9c447340d5d70814a500b4c838c65387
holdenhughes <holdenmh121489@gmail.com>
2015-10-15 14:37:48 -0400

    Broken commit

--- src/Countries.java
+++ src/Countries.java
@@ -1,5 +1,64 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Countries {
+
+
     public static void main(String[] args) {
+        HashMap<String, ArrayList<String>> countryGroup = new HashMap();
+        String postContent = readFile("countries.txt");
+        String[] lines = postContent.split("\n");
+
+
+        for (String line : lines) {
+            String[] columns = line.split("\\|");
+            String firstLetter = String.valueOf(line.charAt(0));
+            ArrayList<String> list = countryGroup.get(firstLetter);
+            String abv = columns[0];
+            String name = columns[1];
+            Country country = new Country(abv, name);
+            countryGroup.put(firstLetter, list);
+
+            if (list == null){
+                list = new ArrayList();
+                country = new Country(abv, name);
+                list.add(country);
+                countryGroup.put(firstLetter, list);
+            } else {
+                list.add(country);
+            }
+        }
+
+
 
     }
+
+
+    static String readFile(String fileName){
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    static void writeFile(String fileName, String fileContent){
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+
+        } catch (Exception e){
+
+        }
+    }
+
 }
---
+++ src/Country.java
@@ -0,0 +1,12 @@
+/**
+ * Created by holdenhughes on 10/15/15.
+ */
+public class Country {
+    String name;
+    String abv;
+
+    public Country(String abv, String name) {
+        this.abv = abv;
+        this.name = name;
+    }
+}
